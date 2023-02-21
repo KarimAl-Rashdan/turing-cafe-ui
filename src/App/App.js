0399175ccb1c9ee229b4c8469d3c5deda6f216d5
karimy <karim.alrashdan14@gmail.com>
2023-02-21 10:22:51 -0700

    Create Form Component

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import fetchData from "../apiCalls"
 import ReservationContainer from "../Components/ReservationContainer/ReservationContainer"
+import Form from "../Components/Form/Form.js"
 
 class App extends Component {
   constructor() {
@@ -14,12 +15,15 @@ class App extends Component {
     fetchData()
     .then((data) => this.setState({reservations: data}))
   }
+  addReservation = (newReservation) => {
+    this.setState({ reservations: [...this.state.reservations, newReservation]})
+  }
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form addReservation={this.addReservation} />
         </div>
         <div className='resy-container'>
           <ReservationContainer reservations={this.state.reservations} />
