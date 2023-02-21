3f9536540a6e0e07eb3e5508215bb734e77f35fc
karimy <karim.alrashdan14@gmail.com>
2023-02-21 10:31:55 -0700

    Complete submitReservation method in Form component

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -11,6 +11,21 @@ class Form extends Component {
       number: null
     }
   }
+  handleChange = event => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+  submitReservation = event => {
+    event.preventDefault()
+    const newReservation = {
+      id: Date.now(),
+      ...this.state
+    }
+    this.props.addReservation(newReservation)
+    this.clearInputs()
+  }
+  clearInputs = () => {
+    this.setState({name: "", date: "", time: "", number: null})
+  }
   render() {
     return (
       <form>
@@ -19,26 +34,30 @@ class Form extends Component {
           placeholder="Name"
           name="name"
           value={this.state.name}
+          onChange={event => this.handleChange(event)}
         />
         <input
           type="text"
           placeholder="Date (mm/dd)"
           name="date"
           value={this.state.date}
+          onChange={event => this.handleChange(event)}
         />
         <input
           type="text"
           placeholder="Time"
           name="time"
           value={this.state.time}
+          onChange={event => this.handleChange(event)}
         />
         <input
           type="number"
           placeholder="Number of guests"
           name="number"
           value={this.state.number}
+          onChange={event => this.handleChange(event)}
         />
-        <button>Make Reservation</button>
+        <button onClick={event => this.submitReservation(event)}>Make Reservation</button>
       </form>
     )
   }
